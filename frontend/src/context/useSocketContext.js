36a39f817ce752c926b109d2ced00d24780b3c5b
Hrahat <hrahat.khachatryan04@mail.ru>
2024-09-29 10:27:26 +0400

    change for socket

--- frontend/src/context/useSocketContext.js
+++ frontend/src/context/useSocketContext.js
@@ -13,7 +13,7 @@ export const SocketContextProvider = ({children}) => {
     const [onlineUsers,setOnlineUsers] =useState([])
     useEffect(() => {
         if (authUser){
-            const socket = io('http://localhost:8080/',{
+            const socket = io('https://mern-social-media-vrmd.onrender.com',{
                 query:{
                     userId: authUser._id
                 }
