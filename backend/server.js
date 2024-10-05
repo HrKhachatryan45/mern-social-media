16eb1f71f229db009a73df644d271e8fa5ab0e2f
Hrahat <hrahat.khachatryan04@mail.ru>
2024-10-05 08:56:18 +0400

    new chnage

--- backend/server.js
+++ backend/server.js
@@ -27,36 +27,39 @@ app.use('/api/auth',authRoutes)
 app.use('/api/profile',profileRoutes)
 app.use('/api/settings',settingsRoutes)
 app.use('/api/messages',messageRoutes)
-app.get('/images/:filename', (req, res) => {
-    try {
-        const filename = req.params.filename;
-        const imagePath = path.join(__dirname, 'uploads', filename);
-        res.sendFile(imagePath);
-    } catch (error) {
-        console.error('Error serving image:', error);
-        res.status(500).send('Error serving image');
-    }
-});
-app.get('/profile/images/:filename', (req, res) => {
-    try {
-        const filename = req.params.filename;
-        const imagePath = path.join(__dirname, 'uploadsP', filename);
-        res.sendFile(imagePath);
-    } catch (error) {
-        console.error('Error serving image:', error);
-        res.status(500).send('Error serving image');
-    }
-});
-app.get('/messages/images/:filename', (req, res) => {
-    try {
-        const filename = req.params.filename;
-        const imagePath = path.join(__dirname, 'uploadsM', filename);
-        res.sendFile(imagePath);
-    } catch (error) {
-        console.error('Error serving image:', error);
-        res.status(500).send('Error serving image');
-    }
-});
+// app.get('/images/:filename', (req, res) => {
+//     try {
+//         const filename = req.params.filename;
+//         const imagePath = path.join(__dirname, 'uploads', filename);
+//         res.sendFile(imagePath);
+//     } catch (error) {
+//         console.error('Error serving image:', error);
+//         res.status(500).send('Error serving image');
+//     }
+// });
+// app.get('/profile/images/:filename', (req, res) => {
+//     try {
+//         const filename = req.params.filename;
+//         const imagePath = path.join(__dirname, 'uploadsP', filename);
+//         res.sendFile(imagePath);
+//     } catch (error) {
+//         console.error('Error serving image:', error);
+//         res.status(500).send('Error serving image');
+//     }
+// });
+// app.get('/messages/images/:filename', (req, res) => {
+//     try {
+//         const filename = req.params.filename;
+//         const imagePath = path.join(__dirname, 'uploadsM', filename);
+//         res.sendFile(imagePath);
+//     } catch (error) {
+//         console.error('Error serving image:', error);
+//         res.status(500).send('Error serving image');
+//     }
+// });
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
+app.use('/profile/images', express.static(path.join(__dirname, 'uploadsP')));
+app.use('/messages/images', express.static(path.join(__dirname, 'uploadsM')));
 
 
 app.get("*", (req, res) => {
